d4f2e30257f349fa287339b32d60f35ad5157897
Muhammad <mabdullahi259@protonmail.com>
2024-03-21 23:59:23 +0000

    'commit'

--- objects_Classes.r
+++ objects_Classes.r
@@ -18,3 +18,24 @@ class(df)
 # models and formulas
 
 y ~ x1 + x2 + x3... + xn  #  formula object.
+
+# dve into the car datase in base r
+
+car.lm <-  lm(formula = dist ~ speed, data = cars)
+car.lm
+
+summary(car.lm)
+qqnorm(residuals(car.lm))
+qqline(residuals(car.lm),
+       col = "red")
+plothist <- hist(residuals(car.lm),
+                 col = "lightblue",
+                 main = "Histogram of Residuals",
+                 xlab = "Residuals")
+abline(v = 0, col = "red")
+plothist
+
+#normality test
+
+shapiro.test(residuals(car.lm))  # p-value > 0.05, residuals are normally, if p-value < 0.05, residuals are not normally
+
